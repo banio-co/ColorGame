3a9aa15cef729a8a7b43a85b0163c090832e4b32
J. Alex Jones <cubepresser@gmail.com>
2022-04-09 11:48:26 -0700

    Another one

--- src/util/model.ts
+++ src/util/model.ts
@@ -2,14 +2,14 @@ export type Dictionary<T> = {
   [key: string]: T;
 };
 
-export type Point = {
+export type Point = Readonly<{
   x: number;
   y: number;
-};
+}>;
 
-export type Polygon = {
+export type Polygon = Readonly<{
   points: Point[];
-}
+}>;
 
 export type Node = {
   id: string;
